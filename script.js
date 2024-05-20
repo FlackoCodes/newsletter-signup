b873c0c40fdea9bdd4fc5eef83fff86d683cf88f
Flacko <lesliejoe2k2@gmail.com>
2024-05-20 23:53:22 +0000

    desktop version

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const errorState = document.querySelector('.error-state')
+
 const emailInput = document.querySelector('.email-field');
 const mainPage = document.querySelector('.main')
 
 const subscribeBtn = document.querySelector('.subscribe-btn');
 const subscribedHtml = document.querySelector('.sub-section');
 
+
+
 subscribeBtn.addEventListener('click', ()=>{
     const clientEmail = emailInput.value;
-    mainPage.classList.add('hide-main');
-    subscribedHtml.classList.add('active-subscribed');
-    console.log(clientEmail);
-    subscribedHtml.innerHTML = `
-    <div id="subscribed-section" class="success">
-      <div class="subscribed">
-        <div>
-          <img src="/assets/images/icon-success.svg" alt="icon-success">
+    if (emailPattern.test(clientEmail)) {
+        mainPage.classList.add('hide-main');
+        subscribedHtml.classList.add('active-subscribed');
+        console.log(clientEmail);
+        subscribedHtml.innerHTML = `
+        <div id="subscribed-section" class="success">
+          <div class="subscribed">
+            <div>
+              <img src="/assets/images/icon-success.svg" alt="icon-success">
+            </div>
+            <div class="text">
+              <h2>Thanks for <span class="subs">subscribing!</span></h2>
+              <p>A confirmation email has been sent to <span class="span-mail">${clientEmail}.</span>
+                Please open it and click the button inside to confirm your subscription.</p>
+            </div>
+          </div>
+          <div class="sub-btn btn">Dismiss message</div>
         </div>
-        <div class="text">
-          <h2>Thanks for <span class="subs">subscribing!</span></h2>
-          <p>A confirmation email has been sent to <span class="span-mail">${clientEmail}.</span>
-            Please open it and click the button inside to confirm your subscription.</p>
-        </div>
-      </div>
-      <div class="btn">Dismiss message</div>
-    </div>
-    `
+        `
+    } else {
+        errorState.classList.add('add-error');
+        emailInput.classList.add('add-email-error')
+    }
+   
 })
